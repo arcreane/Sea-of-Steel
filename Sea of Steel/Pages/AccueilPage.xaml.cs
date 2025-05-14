33fd17827a06f0c990b95bcd107935eec59ce6e2
Squelele <141148037+Squelele@users.noreply.github.com>
2025-05-14 19:00:38 +0200

    ajout easter egg avec secousse

--- Sea of Steel/Pages/AccueilPage.xaml.cs
+++ Sea of Steel/Pages/AccueilPage.xaml.cs
@@ -1,13 +1,18 @@
 using Plugin.Maui.Audio;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Devices.Sensors;
+using Microsoft.Maui.Dispatching;
+using SeaOfSteel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SeaOfSteel.Pages
 {
     public partial class AccueilPage : ContentPage
     {
         private readonly IAudioManager _audioManager;
-        private IAudioPlayer _player;
+        private IAudioPlayer? _player;
         private bool easterEggTriggered = false;
 
         public AccueilPage()
@@ -38,7 +43,7 @@ namespace SeaOfSteel.Pages
             }
         }
 
-        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        private void Accelerometer_ReadingChanged(object? sender, AccelerometerChangedEventArgs e)
         {
             var data = e.Reading;
 
@@ -54,14 +59,20 @@ namespace SeaOfSteel.Pages
 
         private async void TriggerEasterEgg()
         {
-            // Changement d’image de fond
-            BackgroundImage.Source = "sousmarin.png";
+            try
+            {
+                BackgroundImageView.Source = "sousmarin.png";
+                await LogoImage.RotateTo(180, 500, Easing.CubicInOut);
 
-            // Affichage et animation des bulles
-            Bubble1.TranslationY = 0;
-            Bubble1.IsVisible = true;
-            await Bubble1.TranslateTo(0, -600, 3000, Easing.CubicOut);
-            Bubble1.IsVisible = false;
+                Bubble1.TranslationY = 0;
+                Bubble1.IsVisible = true;
+                await Bubble1.TranslateTo(0, -600, 3000, Easing.CubicOut);
+                Bubble1.IsVisible = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur dans l'easter egg : {ex.Message}");
+            }
         }
 
         private async void OnIntroTapped(object sender, EventArgs e)
@@ -89,17 +100,23 @@ namespace SeaOfSteel.Pages
 
         private async void OnJoinClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new LobbyPage(joinMode: true));
+            await Navigation.PushAsync(new LobbyPage()); // Ajoute un paramètre si besoin
         }
 
         private async void OnCreateClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new LobbyPage(joinMode: false));
+            await Navigation.PushAsync(new LobbyPage()); // Ajoute un paramètre si besoin
         }
 
         private void OnQuitClicked(object sender, EventArgs e)
         {
+#if ANDROID
+            Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+#elif WINDOWS
+            Application.Current.Quit();
+#else
             System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+#endif
         }
     }
 }
