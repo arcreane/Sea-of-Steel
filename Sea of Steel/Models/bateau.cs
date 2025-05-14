7331371d176a13c1de7174e043bb7c007a29effe
Squelele <141148037+Squelele@users.noreply.github.com>
2025-05-14 18:59:38 +0200

    ajout d'images

---
+++ Sea of Steel/Models/bateau.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SeaOfSteel.Models;
+
+public class Bateau
+{
+    public string Nom { get; set; }
+    public int Taille { get; set; }
+    public List<(int Row, int Col)> Positions { get; set; } = new();
+
+    public bool EstCoule => Positions.Count == 0;
+}
