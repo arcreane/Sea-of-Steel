7bf33a06a2404ae3c89d88dc07bc611165706498
Squelele <141148037+Squelele@users.noreply.github.com>
2025-05-14 20:52:32 +0200

    au secours

---
+++ Sea of Steel/Sea Of Steel/Platforms/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Sea_Of_Steel
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
---
+++ Sea of Steel/Sea Of Steel/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Sea_Of_Steel
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
