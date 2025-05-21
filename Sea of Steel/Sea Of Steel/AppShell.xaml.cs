d17b022157bb04ec128b1ff61e626f188b88de79
Squelele <141148037+Squelele@users.noreply.github.com>
2025-05-22 00:14:02 +0200

    ajout de wikipage

--- Sea of Steel/Sea Of Steel/AppShell.xaml.cs
+++ Sea of Steel/Sea Of Steel/AppShell.xaml.cs
@@ -7,6 +7,7 @@
             InitializeComponent();
 
             Routing.RegisterRoute("AccueilPage", typeof(Pages.AccueilPage));
+            Routing.RegisterRoute("WikiPage", typeof(Pages.WikiPage));
             Routing.RegisterRoute("LobbyPage", typeof(Pages.LobbyPage));
             Routing.RegisterRoute("JeuPage", typeof(Pages.JeuPage));
             Routing.RegisterRoute("ResultatsPage", typeof(Pages.ResultatsPage));
